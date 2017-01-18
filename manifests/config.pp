d2ba6d18b9f76026d505231de477514259a6cb89
Cosmo Petrich <cosmo.petrich@invoice2go.com>
2017-01-19 10:26:20 +1100

    Use Puppet3-compatible resource syntax
    This doesn't rely on File {} defaults to avoid potential scope
    weirdness, however unlikely.

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,22 +7,24 @@ class telegraf::config inherits telegraf {
   assert_private()
 
   file {
-    default:
-      owner   => 'telegraf',
-      group   => 'telegraf',
-      notify  => Class['::telegraf::service'],
-      require => Class['::telegraf::install'],
-    ;
     $::telegraf::config_file:
       ensure  => file,
       content => template('telegraf/telegraf.conf.erb'),
+      owner   => 'telegraf',
+      group   => 'telegraf',
       mode    => '0640',
+      notify  => Class['::telegraf::service'],
+      require => Class['::telegraf::install'],
     ;
     $::telegraf::config_fragment_dir:
       ensure  => directory,
+      owner   => 'telegraf',
+      group   => 'telegraf',
       mode    => '0750',
       purge   => $::telegraf::purge_config_fragments,
       recurse => true,
+      notify  => Class['::telegraf::service'],
+      require => Class['::telegraf::install'],
     ;
   }
 
