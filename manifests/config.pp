0d981f671cd2e70fc0ced5254f8008fbae583f18
Simon Hoenscheid <code@simonhoenscheid.de>
2019-02-25 15:51:08 +0100

    sync PR with upstream

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,22 +6,20 @@ class telegraf::config inherits telegraf {
 
   assert_private()
 
-  file {
-    $::telegraf::config_file:
-      ensure  => file,
-      content => template('telegraf/telegraf.conf.erb'),
-      owner   => $::telegraf::config_file_owner,
-      group   => $::telegraf::config_file_group,
-      mode    => $::telegraf::config_file_mode,
-    ;
-    $::telegraf::config_folder:
-      ensure  => directory,
-      owner   => $::telegraf::config_file_owner,
-      group   => $::telegraf::config_file_group,
-      mode    => $::telegraf::config_folder_mode,
-      purge   => $::telegraf::purge_config_fragments,
-      recurse => true,
-    ;
+  file { $telegraf::config_file:
+    ensure  => file,
+    content => template('telegraf/telegraf.conf.erb'),
+    owner   => $telegraf::config_file_owner,
+    group   => $telegraf::config_file_group,
+    mode    => $telegraf::config_file_mode,
+  }
+  file { $telegraf::config_folder:
+    ensure  => directory,
+    owner   => $telegraf::config_file_owner,
+    group   => $telegraf::config_file_group,
+    mode    => $telegraf::config_folder_mode,
+    purge   => $telegraf::purge_config_fragments,
+    recurse => true,
   }
 
 }
