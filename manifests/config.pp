6a14b4ff87d21a072588ae312a6ec14bb5b5d2bb
Stefan Warten <sw@00101010.org>
2019-03-07 16:35:07 +0100

    Moved selector out of resource block

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,21 +6,23 @@ class telegraf::config inherits telegraf {
 
   assert_private()
 
+  $config_file_ensure = $telegraf::ensure ? {
+    'absent' => absent,
+    default  => file,
+  }
+  $config_folder_ensure = $telegraf::ensure ? {
+    'absent' => absent,
+    default  => directory,
+  }
   file { $telegraf::config_file:
-    ensure  => $telegraf::ensure ? {
-        'absent' => absent,
-        default  => file,
-    },
+    ensure  => $config_file_ensure,
     content => template('telegraf/telegraf.conf.erb'),
     owner   => $telegraf::config_file_owner,
     group   => $telegraf::config_file_group,
     mode    => '0640',
   }
   file { $telegraf::config_folder:
-    ensure  => $telegraf::ensure ? {
-        'absent' => absent,
-        default  => directory,
-    },
+    ensure  => $config_folder_ensure,
     owner   => $telegraf::config_file_owner,
     group   => $telegraf::config_file_group,
     mode    => '0770',
