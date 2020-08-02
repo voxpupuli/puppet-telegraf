00cb997f734ec71f27043013b849866b038c2708
Tim Meusel <tim@bastelfreak.de>
2020-08-02 18:29:43 +0200

    fix broken spec test

--- spec/defines/aggregator_spec.rb
+++ spec/defines/aggregator_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'telegraf::aggregator' do
-  on_supported_os(facterversion: '3.6').each do |os, facts|
+  on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
         facts
