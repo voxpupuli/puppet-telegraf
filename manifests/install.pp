95749f76b0d46680d56ad2ddc54c9d07ed577fb2
Marcel Kurz <kurz@eventit.ag>
2021-05-10 10:56:56 +0200

    Revert "change key id to new pub key"
    This reverts commit 2671cf267e35c3054cbcc2711877eddb529d5224.

--- manifests/install.pp
+++ manifests/install.pp
@@ -96,7 +96,7 @@ class telegraf::install {
           release  => $release,
           repos    => $telegraf::repo_type,
           key      => {
-            'id'     => 'A4B94A8C145F4C456133EFEFAB162277E8EE6FD0',
+            'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
             'source' => "${telegraf::repo_location}influxdb.key",
           },
         }
