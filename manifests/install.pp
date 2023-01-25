51f1a2f99290707bab7a774215b1e328daf63daf
Josh Powers <powersj@fastmail.com>
2023-01-25 07:33:13 -0700

    rename key file, remove -oss

--- manifests/install.pp
+++ manifests/install.pp
@@ -103,7 +103,7 @@ class telegraf::install {
           repos    => $telegraf::repo_type,
           key      => {
             'id'     => '9D539D90D3328DC7D6C8D3B9D8FF8E1F7DF8B07E',
-            'source' => "${telegraf::repo_location}influxdata-oss-archive_compat.key",
+            'source' => "${telegraf::repo_location}influxdata-archive_compat.key",
           },
         }
         Class['apt::update'] -> Package[$telegraf::package_name]
